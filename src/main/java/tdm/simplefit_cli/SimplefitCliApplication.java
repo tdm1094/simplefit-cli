5188f9e25987a9239a927b6453e9a0ab96da859a
Tomás Marmoni <tdm1094@gmail.com>
2024-08-02 20:33:03 -0300

    feat: Agregado proyecto base hecho con Spring Boot 3.3.2

---
+++ src/main/java/tdm/simplefit_cli/SimplefitCliApplication.java
@@ -0,0 +1,13 @@
+package tdm.simplefit_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplefitCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplefitCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdm/simplefit_cli/SimplefitCliApplicationTests.java
@@ -0,0 +1,13 @@
+package tdm.simplefit_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplefitCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
